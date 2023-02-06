c55da6bfd5429942affb3e360879702e3bb8202f
SC <sunweichao@rayclear.com>
2023-02-06 14:53:38 +0800

    Initial Commit

---
+++ EveryoneSpeaks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EveryoneSpeaks
+//
+//  Created by 孙超 on 2023/2/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
