13abb6b9740c15ac9abff256c079aad07a1b8d6e
SC <sunweichao@rayclear.com>
2023-02-07 11:32:00 +0800

    add ignore

---
+++ AppConfig/AppConfig.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfig.swift
+//  EveryoneSpeaks
+//
+//  Created by 孙超 on 2023/2/7.
+//
+
+import UIKit
+
+/// 用于布局的常量
+struct LayoutConstantConfig
+{
+    /// 屏幕宽度
+    static var screenWidth = UIScreen.main.bounds.size.width
+    /// 屏幕高度
+    static let screenHeight = UIScreen.main.bounds.size.height
+}
--- Launch/AppDelegate.swift
+++ Launch/AppDelegate.swift
@@ -10,13 +10,32 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
+    var window: UIWindow?
 
-
+    static var shared: AppDelegate? {
+        return UIApplication.shared.delegate as? AppDelegate
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        guard #unavailable(iOS 13) else
+        {
+            return true
+        }
+        window = UIWindow(frame: .init(x: 0, y: 0, width:LayoutConstantConfig.screenWidth , height: LayoutConstantConfig.screenHeight))
         return true
     }
 
+    
+
+
+}
+
+@available (iOS 13,*)
+extension AppDelegate
+{
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -30,7 +49,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
