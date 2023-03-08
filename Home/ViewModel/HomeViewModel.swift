2bcd48f0a08c3ba02077a99c8e353a80cfd8a93e
孙超 <sunweichao@rayclear.com>
2023-03-08 18:32:40 +0800

    add ui

---
+++ Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  HomeViewModel.swift
+//  EveryoneSpeaks
+//
+//  Created by SC on 2023/3/8.
+//
+
+import Foundation
+final class HomeViewModel: ViewModelProjectType
+{
+    struct Input {
+        
+    }
+    
+    struct Output
+    {
+        
+    }
+    
+    var input: Input
+    var output: Output
+    
+    init()
+    {
+        input = Input()
+        output = Output()
+    }
+}
