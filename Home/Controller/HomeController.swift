22f03c056776fc97f643943050fd7066a993b6ab
孙超 <sunweichao@rayclear.com>
2023-03-08 18:24:15 +0800

    add ui

--- Home/Controller/HomeController.swift
+++ Home/Controller/HomeController.swift
@@ -14,6 +14,8 @@ class HomeController: BaseController {
     override func viewDidLoad() {
         super.viewDidLoad()
         provider.rx.request(.channelClassification).subscribe(onSuccess: {response in
+           let array =  response.data.kj.modelArray(ChannelClassificationModel.self)
+            DDLogDebug(array)
             if let json = try? response.mapJSON()
             {
                 DDLogDebug(json)
---
+++ Home/Model/ChannelClassificationModel.swift
@@ -0,0 +1,16 @@
+//
+//  ChannelClassificationModel.swift
+//  EveryoneSpeaks
+//
+//  Created by SC on 2023/3/8.
+//
+
+import UIKit
+
+class ChannelClassificationModel: BaseModel {
+    var id = 0
+    var background = ""
+    var logo = ""
+    var name = ""
+    var shortname = ""
+}
